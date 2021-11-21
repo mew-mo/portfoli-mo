64ba11365e1031e0044a1ada8b9a56d3713202d9
mo <tickle.monique@gmail.com>
2021-11-21 17:55:13 +1300

    all pages r here, responsive n contact work nxt :turtle:

--- js/custom.js
+++ js/custom.js
@@ -78,14 +78,80 @@
   // ==================
 
   var graphic = [
-
+    luma = {
+      id: 'luma',
+      title: 'Luma Lights Festival',
+      img: 'img/luma.png',
+      link: 'https://drive.google.com/uc?id=16zQq6fEFpG9rv4Gv9XyzFCgPrpCltInM',
+      brief: 'Branding for Luma Lights Festival',
+      tools: ['photoshop', 'illustrator'],
+      description: 'A design project in which I created branding, posters and cards to promote Queenstown\'s Luma Lights Festival.'
+    },
+    dlw = {
+      id: 'dlw',
+      title: 'Don\'t Let Words',
+      img: 'img/dlw.png',
+      link: '#',
+      brief: 'Branding for an anti-1080 Campaign',
+      tools: ['photoshop', 'illustrator'],
+      description: 'This design project utilised the tagline "Don\'t Let Words be all that\'s left of them" to campaign against 1080 poisoning in New Zealand. I created posters, logos, brochures and merchandise for this project.'
+    },
+    mmca = {
+      id: 'mmca',
+      title: 'MMCA',
+      img: 'img/mmca.png',
+      link: '#',
+      brief: 'Brochure for the MMCA Museum',
+      tools: ['photoshop', 'indesign'],
+      description: 'This project was for designing a brochure for the MMCA- The National Museum of Modern and Contemporary Art, located in South Korea. It is primarily focused on navigation, and features a large cutout triangle that makes the map display even when the brochure is fully folded.'
+    },
+    tinyHouse = {
+      id: 'tinyhouse',
+      title: 'Tiny House',
+      img: 'img/tinyhouse.png',
+      link: '#',
+      brief: 'Fictional Logo for Tiny House',
+      tools: ['photoshop', 'indesign'],
+      description: 'A small branding project, where I was challenged to create a logo for a fictional company that writes magazines about small houses.'
+    },
+    musicPlanet = {
+      id: 'musicPlanet',
+      title: 'Music Planet',
+      img: 'img/musicplanet.png',
+      link: 'https://drive.google.com/uc?id=1rSmCQEYOcSRg9iNA3HUVuXdH6vt8ySkx',
+      brief: 'Logo Simplification & Motion Graphic for Music Planet',
+      tools: ['photoshop', 'illustrator', 'indesign'],
+      description: 'A project in which I simplified the current logo of Music Planet through user testing and created a graphic for them via animating in Photoshop.'
+    },
+    alphabet = {
+      id: 'alphabet',
+      title: 'Maori Alphabet',
+      img: 'img/alphabet.png',
+      link: 'https://drive.google.com/uc?id=1QjWa76DebVS1aXojGLTiPZVuxXitumvu',
+      brief: 'An alphabet chart for Maori Language Week',
+      tools: ['photoshop', 'illustrator'],
+      description: 'A project made in collaboration with Rane Magno. We created this alphabet chart for Maori Language Week with the theme of constellations and stars, which are of huge cultural importance to Maori people.'
+    },
+    snowdin = {
+      id: 'snowdin',
+      title: 'Snowdin Snacks',
+      img: 'img/snowdin.png',
+      link: '#',
+      brief: 'A packaging project',
+      tools: ['photoshop', 'illustrator', 'physical skills'],
+      description: 'Inspired by Toby Fox\'s "Undertale", I created a fictional company entitled "Snowdin Snacks" and designed the packaging for the frozen treat called the "Bisicle". The packaging was created physically through researching substrates and both creating my own die-lines and cutting them out.'
+    }
   ];
 
   // digital art arr
   // ==================
 
-  var digart = [
-
+  var digiArt = [
+    tentwentyfive = {
+      id: '1025',
+      title: '1025',
+      img: 'img/1025.png'
+    }
   ];
 
 
@@ -210,14 +276,202 @@
       document.querySelector('.proj-img img').src = appWuxProj.selectedPage.img;
 
       document.title += `  ${appWuxProj.selectedPage.title}`;
-
-
-      // document.querySelector('#wuxProj').innerHTML += `We got the ${appWuxProj.selectedPage.title}`;
     }
-
-
   }; //appwuxproj ends
 
+  // GRAPHIC.HTML
+  // ==========================================
+  var appGraphic = {
+
+    bubbles: [],
+
+    init: () => {
+      appGraphic.addProjs();
+      appGraphic.click();
+    },
+
+    addProjs: () => {
+      for (var i = 0; i < graphic.length; i++) {
+        document.querySelector('.work-bbls').innerHTML += `
+        <div class="bubble-container">
+            <div id="bubble${i}" class="bubble">
+            <a href="#" id="${graphic[i].id}" class="project">
+              <h5 class="light-pal">${graphic[i].title}</h5>
+                <img src="${graphic[i].img}" alt="${graphic[i].title} Project Preview"></a>
+            </div>
+        </div>
+        `;
+
+        appGraphic.bubbles.push(`#bubble${i}`) ;
+      } //for loop ends
+
+      setInterval(appGraphic.anims, 1000);
+
+      appGraphic.anims();
+    },
+
+    anims: () => {
+      var container = document.querySelector('.work-bbls');
+
+      var height = container.offsetHeight;
+      var width = container.offsetWidth;
+
+      for (var i = 0; i < appGraphic.bubbles.length; i++) {
+        anime({
+          targets: `${appGraphic.bubbles[i]}`,
+          translateY: Math.floor(Math.random() * height),
+          translateX: Math.floor(Math.random() * width),
+          loop: false,
+          direction: 'alternate',
+          easing: 'easeInOutCirc'
+        });
+      }
+    }, //anims
+
+    click: () => {
+      window.addEventListener('click', (e) => {
+        if (e.target.parentNode.className == 'project') {
+          sessionStorage.setItem('selected', e.target.parentNode.id);
+          window.location.href = 'graphic-proj.html';
+        } //if e target
+      }, false);
+    }
+  }; //appGraphic ends
+
+  // GRAPHIC-PROJ.HTML
+  // ==========================================
+  appGraphicProj = {
+
+    selectedPage: false,
+
+    init: () => {
+      appGraphicProj.setup();
+    },
+
+    setup: () => {
+      for (var i = 0; i < graphic.length; i++) {
+        if (graphic[i].id == sessionStorage.selected) {
+          appGraphicProj.selectedPage = graphic[i];
+        } //if
+      } //for
+
+      document.querySelector('.proj-title').innerHTML = appGraphicProj.selectedPage.title;
+
+      document.querySelector('.proj-brief').innerHTML = appGraphicProj.selectedPage.brief;
+
+      document.querySelector('.proj-tools').innerHTML = appGraphicProj.selectedPage.tools;
+
+      document.querySelector('.proj-desc').innerHTML = appGraphicProj.selectedPage.description;
+
+      document.querySelector('.go-to-proj-btn a').href = appGraphicProj.selectedPage.link;
+
+      document.querySelector('.proj-img img').src = appGraphicProj.selectedPage.img;
+
+      document.title += `  ${appGraphicProj.selectedPage.title}`;
+    }
+  };
+
+  // DIGIART.HTML
+  // ==========================================
+  var appDigi = {
+
+    selectedArt: false,
+
+    bubbles: [],
+
+    init: () => {
+      appDigi.addProjs();
+      appDigi.click();
+    },
+
+    addProjs: () => {
+      for (var i = 0; i < digiArt.length; i++) {
+        document.querySelector('.work-bbls').innerHTML += `
+        <div class="bubble-container">
+            <div id="bubble${i}" class="bubble">
+            <a href="#" id="${digiArt[i].id}" class="project">
+              <h5 class="light-pal">${digiArt[i].title}</h5>
+                <img src="${digiArt[i].img}" alt="${digiArt[i].title} Project Preview"></a>
+            </div>
+        </div>
+        `;
+
+        appDigi.bubbles.push(`#bubble${i}`) ;
+      } //for loop ends
+
+      setInterval(appDigi.anims, 1000);
+
+      appDigi.anims();
+    },
+
+    anims: () => {
+      var container = document.querySelector('.work-bbls');
+
+      var height = container.offsetHeight;
+      var width = container.offsetWidth;
+
+      for (var i = 0; i < appDigi.bubbles.length; i++) {
+        anime({
+          targets: `${appDigi.bubbles[i]}`,
+          translateY: Math.floor(Math.random() * height),
+          translateX: Math.floor(Math.random() * width),
+          loop: false,
+          direction: 'alternate',
+          easing: 'easeInOutCirc'
+        });
+      }
+    }, //anims
+
+    click: () => {
+      window.addEventListener('click', (e) => {
+        if (e.target.parentNode.className == 'project') {
+          sessionStorage.setItem('selected', e.target.parentNode.id);
+
+          appDigi.setup();
+
+        } //if e target
+      }, false);
+    },
+
+    setup: () => {
+      for (var i = 0; i < digiArt.length; i++) {
+        if (digiArt[i].id == sessionStorage.selected) {
+          appDigi.selectedArt = digiArt[i];
+        } //if
+      } //for
+
+      document.querySelector('.section-img-overlay').style.display = 'flex';
+
+      document.querySelector('body').style.overflowY = 'hidden';
+
+      document.querySelector('nav').style.height = '0';
+
+      document.querySelector('.digi-title').innerHTML = `"${appDigi.selectedArt.title}"`;
+
+      document.querySelector('.digi-img').src = appDigi.selectedArt.img;
+
+      document.querySelector('.digi-img').alt = appDigi.selectedArt.title;
+
+      document.querySelector('.digi-back').addEventListener('click', () => {
+        document.querySelector('nav').style.height = '100px';
+        document.querySelector('.section-img-overlay').style.display = 'none';
+        document.querySelector('body').style.overflowY = 'auto';
+
+      }, false);
+    }
+  }; //appDigi ends
+
+  appAbt = {
+    init: () => {
+      anime({
+        targets: '.circle-container',
+        translateY: 20,
+        direction: 'alternate',
+        loop: true,
+        easing: 'easeInOutSine'
+      });
+    }
+  };
 
   if (document.querySelector('#home')) {
     appHome.init();
@@ -225,6 +479,14 @@
     appWux.init();
   } else if (document.querySelector('#wuxProj')) {
     appWuxProj.init();
+  } else if (document.querySelector('#graphic')) {
+    appGraphic.init();
+  } else if (document.querySelector('#graphicProj')) {
+    appGraphicProj.init();
+  } else if (document.querySelector('#digiArt')) {
+    appDigi.init();
+  } else if (document.querySelector('#about')) {
+    appAbt.init();
   }
 
 }());
